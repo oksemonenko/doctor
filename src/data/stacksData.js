f1168c192cc9ca77bd549d3b1ea73ffc6ac2e5ca
Oksana <ok.semonenko@gmail.com>
2020-04-11 02:45:10 +0300

    add data

---
+++ src/data/stacksData.js
@@ -0,0 +1,49 @@
+import type1One from '../img/type1/1.jpeg'
+import type2One from '../img/type2/1.jpeg'
+import type3One from '../img/type3/1.jpeg'
+import type4One from '../img/type4/1.jpeg'
+
+const stacksData = [
+  {
+    title: 'Doctor',
+    items: [
+      {
+        image: type1One,
+        name: '',
+        description: '',
+      }
+    ]
+  },
+  {
+    title: 'Músico',
+    items: [
+      {
+        image: type2One,
+        name: '',
+        description: '',
+      }
+    ]
+  },
+  {
+    title: 'Hombre simple',
+    items: [
+      {
+        image: type3One,
+        name: '',
+        description: '',
+      }
+    ]
+  },
+  {
+    title: 'Superhéroe',
+    items: [
+      {
+        image: type4One,
+        name: '',
+        description: '',
+      }
+    ]
+  },
+];
+
+export default stacksData;

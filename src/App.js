14a9dd86b7a9ff34e9df0fc38f1b894603e077d2
Oksana <ok.semonenko@gmail.com>
2020-04-11 03:54:44 +0300

    add Hero component and main image transform effect

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import './App.css';
 import Slider from './components/Slider';
+import Hero from './components/Hero';
 import stacksData from './data/stacksData'
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <div className="hero">
-          <div className="hero__back hero__back--static"></div>
-          <div className="hero__back hero__back--mover"></div>
-          <div className="hero__front"></div>
-        </div>
+        <Hero />
         <header className="codrops-header">
           <h1 className="codrops-title">Dr. Jhey <span>El arte de la simplicidad</span></h1>
         </header>
---
+++ src/components/Hero.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import {throttle} from 'lodash';
+
+export default class Hero extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      style: {
+        WebkitTransform: null,
+        transform: null,
+      }
+    };
+    window.addEventListener('mousemove', this.onMouseMove);
+  }
+
+  onMouseMove = throttle ((ev) => {
+    const win = { width: window.innerWidth, height: window.innerHeight };
+
+    const xVal = -1/(win.height/2)*ev.clientY + 1;
+    const yVal = 1/(win.width/2)*ev.clientX - 1;
+    const transX = 20/(win.width)*ev.clientX - 10;
+    const transY = 20/(win.height)*ev.clientY - 10;
+    const transZ = 100/(win.height)*ev.clientY - 50;
+    const transform = 'perspective(1000px) translate3d(' + transX + 'px,' + transY + 'px,' + transZ + 'px) rotate3d(' + xVal + ',' + yVal + ',0,2deg)';
+
+    this.setState({ style: {
+      WebkitTransform: transform,
+      transform,
+    }})
+  }, 100);
+
+  render() {
+    const { style } = this.state;
+    return (
+      <div className="hero">
+        <div className="hero__back hero__back--static"></div>
+        <div className="hero__back hero__back--mover" style={style}></div>
+        <div className="hero__front"></div>
+      </div>
+    );
+  }
+}
